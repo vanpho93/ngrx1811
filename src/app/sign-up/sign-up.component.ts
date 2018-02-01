25fa6cd02f47f9e9d972ed948916609c08071c40
Van Pho Nguyen <vanpho02@gmail.com>
2018-02-01 20:19:35 +0700

    file

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -10,7 +9,7 @@ export class SignUpComponent implements OnInit {
   formSignUp: FormGroup;
   constructor() {
     this.formSignUp = new FormGroup({
-      email: new FormControl(''),
+      email: new FormControl('', Validators.email),
       name: new FormControl(''),
       password: new FormControl(''),
       rePassword: new FormControl(''),
@@ -21,4 +20,7 @@ export class SignUpComponent implements OnInit {
   ngOnInit() {
   }
 
+  signUp() {
+    console.log(this.formSignUp.value);
+  }
 }
